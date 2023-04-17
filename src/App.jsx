21040905d3662e5abfd803d8c4bee04e9e1e5043
Vale Quiroga <90653297+ValeQRamos@users.noreply.github.com>
2023-04-17 08:42:25 -0500

    adding react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Hero />
-      <ItemListContainer greeting="Main Title Soon..." />
-      <ItemDetailContainer />
-    </>
+      <Routes>
+        <Route path="/" element={<ItemListContainer greeting="Main Title Soon..." />} />
+        <Route path="/detail" element={<ItemDetailContainer />} />
+      </Routes>
+    </Router>
   );
 }
 

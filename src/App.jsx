33cf9515246e54e9db27d2e8b8db890946576a95
Vale Quiroga <90653297+ValeQRamos@users.noreply.github.com>
2023-05-08 15:37:16 -0500

    starting checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import NotFound from "./components/NotFound/NotFound";
 import CartContainer from "./components/CartContainer/CartContainer";
+import Checkout from "./components/Checkout/Checkout";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           />
           <Route path="/detail/:id" element={<ItemDetailContainer />} />
           <Route path="/cart" element={<CartContainer />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
---
+++ src/components/Checkout/Checkout.jsx
@@ -0,0 +1,9 @@
+const Checkout = () => {
+  return (
+    <div>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <h1> Checkout </h1>
+    </div>
+  );
+};
+export default Checkout;

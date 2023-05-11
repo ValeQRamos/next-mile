75c0f23b3bd06d4ad6f3912715be397dcafada15
Vale Quiroga <90653297+ValeQRamos@users.noreply.github.com>
2023-05-11 15:05:41 -0500

    adding a second parallax

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,30 @@
 import "./Hero.css";
 import { Parallax } from "react-parallax";
 import running1 from "../../../public/images/running.jpg";
+import running2 from "../../../public/images/runningTwo.png";
 const Hero = () => {
   return (
-    <Parallax strength={300} blur={{ min: -15, max: 15 }} bgImage={running1}>
-      <div className="content">
-        <div className="text-content">
-          <h1>Welcome To Next Mile</h1>
-          <p> The best zero-drop running shoes in town. </p>
+    <>
+      <Parallax strength={200} blur={{ min: -20, max: 15 }} bgImage={running1}>
+        <div className="content">
+          <div className="text-content">
+            <h1>Welcome To Next Mile</h1>
+            <p> The best zero-drop running shoes in town. </p>
+          </div>
         </div>
-      </div>
-    </Parallax>
+      </Parallax>
+      <Parallax strength={200} blur={{ min: -20, max: 15 }} bgImage={running2}>
+        <div className="content">
+          <div className="text-content">
+            <div className="h2-content">
+              <h2>Running</h2>
+              <h2>Better</h2>
+              <h2>Everyday</h2>
+            </div>
+          </div>
+        </div>
+      </Parallax>
+    </>
   );
 };
 export default Hero;

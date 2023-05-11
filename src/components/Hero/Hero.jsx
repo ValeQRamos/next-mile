e37cb981fd1295a7f919483a8099c8bb13b9eb5e
Vale Quiroga <90653297+ValeQRamos@users.noreply.github.com>
2023-05-11 10:42:57 -0500

    adding new branch

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="container">
-        <h1>Welcome To Next Mile</h1>
+        <h1>Welcome To Next Mile.</h1>
         <p>
           The best zero-drop running shoes in town.
         </p>
